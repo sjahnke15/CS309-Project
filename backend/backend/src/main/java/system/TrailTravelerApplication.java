dd8f2b4bbf81a57f77a084190d5cb215d7acaff9
Sullivan Jahnke <sjahnke@iastate.edu>
2021-09-28 17:20:47 -0500

    Created controller, repo interface, and class for "Trails"
    This will be used to interact with the Trails table in our DB after it is connected

---
+++ backend/backend/src/main/java/system/TrailTravelerApplication.java
@@ -0,0 +1,12 @@
+package system;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailTravelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailTravelerApplication.class, args);
+	}
+
+}
