efb92deaa61769b4a925d1484b429346c3528112
Erik <esraman@iastate.edu>
2021-10-04 14:37:36 -0500

    Corrected postmapping to include email

--- backend/backend/src/main/java/coms309/trailtraveler/backend/controller/UserController.java
+++ backend/backend/src/main/java/coms309/trailtraveler/backend/controller/UserController.java
@@ -25,7 +25,6 @@ public class UserController {
 	@GetMapping("user/username/{username}")
 	User getUserByUsername(@PathVariable String username) {
 		List<User> users = UserRepository.findAll();
-		
 		User u;
 		for(int i = 0; i < users.size(); i++) {
 			if(users.get(i).getUsername().equals(username)) {
@@ -33,13 +32,26 @@ public class UserController {
 				return u;
 			}
 		}
-		
 		return null;
 	}
 	
-	@PostMapping("user/post/{n}/{p}")
-	User postUserByPath(@PathVariable String n, @PathVariable String p) {
+	@GetMapping("user/email/{email}")
+	User getUserByEmail(@PathVariable String email) {
+		List<User> users = UserRepository.findAll();
+		User u;
+		for(int i = 0; i < users.size(); i++) {
+			if(users.get(i).getEmail().equals(email)) {
+				u = users.get(i);
+				return u;
+			}
+		}
+		return null;
+	}
+	
+	@PostMapping("user/post/{e}/{n}/{p}")
+	User postUserByPath(@PathVariable String e, @PathVariable String n, @PathVariable String p) {
 		User u = new User();
+		u.setEmail(e);
 		u.setUsername(n);
 		u.setPassword(p);
 		UserRepository.save(u);
