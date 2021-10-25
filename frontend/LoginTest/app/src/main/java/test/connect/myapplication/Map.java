1b41328a67c089ce35f0442bc353f494c012b695
Danny Chrisman <dannychrisman@Dannys-MacBook-Pro.local>
2021-10-25 17:11:17 -0500

    Forgot functionality between 2 screens

--- frontend/LoginTest/app/src/main/java/test/connect/myapplication/Map.java
+++ frontend/LoginTest/app/src/main/java/test/connect/myapplication/Map.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 public class Map extends AppCompatActivity {
 Button toHome;
 Button toWildlife;
+Button toTrailList;
+Button toTrailInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +32,24 @@ Button toWildlife;
             public void onClick(View view) {
                 Intent wildlife = new Intent(Map.this, Wildlife.class);
                 startActivity(wildlife);
+            }
+        });
+
+        toTrailList = (Button)findViewById(R.id.btnToTrailList);
+        toTrailList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toTrailList = new Intent(Map.this, TrailList.class);
+                startActivity(toTrailList);
+            }
+        });
+
+        toTrailInfo = (Button)findViewById(R.id.btnToTrailInfo);
+        toTrailInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toTrailInfo = new Intent(Map.this, TrailInfo.class);
+                startActivity(toTrailInfo);
             }
         });
     }
--- frontend/LoginTest/app/src/main/java/test/connect/myapplication/TrailList.java
+++ frontend/LoginTest/app/src/main/java/test/connect/myapplication/TrailList.java
@@ -2,13 +2,25 @@ package test.connect.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TrailList extends AppCompatActivity {
-
+Button backToMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trail_list2);
+
+        backToMap = (Button)findViewById(R.id.btnBackToMap);
+        backToMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back = new Intent(TrailList.this, Map.class);
+                startActivity(back);
+            }
+        });
     }
 }
