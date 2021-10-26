29d55f8537975c49c952024b9da53a2256201166
Sullivan Jahnke <sjahnke@iastate.edu>
2021-10-26 18:02:42 -0500

    Added a Get method to return all reviews for a trail
    The method takes in the trail ID, and sends all reviews associated with that trail in a list

--- backend/backend/src/main/java/coms309/trailtraveler/backend/controller/TrailController.java
+++ backend/backend/src/main/java/coms309/trailtraveler/backend/controller/TrailController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import coms309.trailtraveler.backend.model.Review;
 import coms309.trailtraveler.backend.model.Trail;
 import coms309.trailtraveler.backend.repository.TrailRepository;
 
@@ -20,6 +21,17 @@ public class TrailController {
 	@GetMapping("trail/all")
 	List<Trail> getAllTrails() {
 		return trailRepository.findAll();
+	}
+	
+	@GetMapping("trail/getReviews/{trailID}")
+	List<Review> getReviewsByTrailID(@PathVariable int trailID) throws Exception {
+		List<Trail> trails = trailRepository.findAll();
+		for(int i = 0; i < trails.size(); i++) {
+			if(trails.get(i).getId() == trailID) {
+				return trails.get(i).getReviews();
+			}
+		}
+		throw new Exception("Trail ID does not exist");
 	}
 	
 	@PostMapping("trail/post/{n}/{d}")
