83aa207ef7176f2259cb10886bf7a6ca0ac2674d
Elijah Hanson <edhanson@iastate.edu>
2021-11-16 15:26:07 -0600

    Proof of concept for Trail list screen

--- frontend/LoginTest/app/src/main/java/test/connect/myapplication/TrailList.java
+++ frontend/LoginTest/app/src/main/java/test/connect/myapplication/TrailList.java
@@ -1,11 +1,18 @@
 package test.connect.myapplication;
 
+import static test.connect.myapplication.api.ApiClientFactory.GetTrailApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+import test.connect.myapplication.api.SlimCallback;
+import test.connect.myapplication.model.Trail;
 
 public class TrailList extends AppCompatActivity {
 Button backToMap;
@@ -23,11 +30,31 @@ Button backToMap;
         Button Trail5 = findViewById(R.id.btnTrail5);
         Button start = findViewById(R.id.btnStartTrail);
 
+        TextView TrailDist1 = findViewById(R.id.txtDistanceTrail1);
+        TextView TrailDiff1 = findViewById(R.id.txtDifficultyTrail1);
+        TextView TrailRating1 = findViewById(R.id.txtRatingTrail1);
+
         Intent intent = getIntent();
         String username = intent.getStringExtra("username");
         String email = intent.getStringExtra("email");
         String password = intent.getStringExtra("password");
         int userID = intent.getIntExtra("userID", 0);
+
+        GetTrailApi().getAllTrails().enqueue(new SlimCallback<List<Trail>>(trails->{
+            Trail1.setText("");
+            for (int i = 0; i < trails.size(); i++){
+                if(i == 0){
+                    Trail1.setText(trails.get(i).getName());
+                    TrailDist1.setText("Placeholder");
+                    TrailDiff1.setText(String.valueOf(trails.get(i).getDifficulty()));
+                    TrailRating1.setText("Placeholder");
+                }
+                else if(i == 1){
+                    Trail2.setText(trails.get(i).getName());
+                }
+            }
+
+        }, "multipleUsersApi"));
 
         backToMap = (Button)findViewById(R.id.btnBackToMap);
         backToMap.setOnClickListener(new View.OnClickListener() {
