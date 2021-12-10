bd81993e01bbdf5757da910877e7a03519c50b94
Sullivan Jahnke <sjahnke@iastate.edu>
2021-12-10 10:57:50 -0600

    Added another trail test for getting trail by name

--- backend/backend/src/test/java/coms309/trailtraveler/backend/TrailTests.java
+++ backend/backend/src/test/java/coms309/trailtraveler/backend/TrailTests.java
@@ -21,7 +21,7 @@ import coms309.trailtraveler.backend.repository.TrailRepository;
 import coms309.trailtraveler.backend.service.TrailService;
 
 @RunWith(SpringRunner.class)
-public class TrailAverageTest {
+public class TrailTests {
 	
 	@TestConfiguration
 	static class TrailContextConfiguration {
@@ -98,4 +98,66 @@ public class TrailAverageTest {
 		assertEquals(5.3, ts.retrieveTrailRating(t.getName()), 0);
 	}
 
+	@Test
+	public void testRetrieveTrailByName() {
+		List<Trail> l = new ArrayList<>();
+		
+		when(tRepo.findAll()).thenReturn(l);
+		when(tRepo.save((Trail)any(Trail.class))).thenAnswer(x -> {
+			Trail r = x.getArgument(0);
+			l.add(r);
+			return null;
+		});
+		
+		Trail t = new Trail();
+		t.setId(0);
+		t.setName("Trail One");
+		Trail t1 = new Trail();
+		t1.setId(1);
+		t1.setName("Trail Two");
+		Trail t2 = new Trail();
+		t2.setId(2);
+		t2.setName("Trail Three");
+		Trail t3 = new Trail();
+		t3.setId(3);
+		t3.setName("Trail Four");
+		Trail t4 = new Trail();
+		t4.setId(4);
+		t4.setName("Trail Five");
+		Trail t5 = new Trail();
+		t5.setId(5);
+		t5.setName("Trail Six");
+		Trail t6 = new Trail();
+		t6.setId(6);
+		t6.setName("Trail Seven");
+		Trail t8 = new Trail();
+		t8.setId(7);
+		t8.setName("Trail Eight");
+		Trail t7 = new Trail();
+		t7.setId(8);
+		t7.setName("Trail Nine");
+		tRepo.save(t);
+		tRepo.save(t1);
+		tRepo.save(t2);
+		tRepo.save(t3);
+		tRepo.save(t4);
+		tRepo.save(t5);
+		tRepo.save(t6);
+		tRepo.save(t7);
+		tRepo.save(t8);
+		
+		//Can get all names in database
+		assertEquals(0, ts.retrieveTrailByName("Trail One").getId());
+		assertEquals(1, ts.retrieveTrailByName("Trail Two").getId());
+		assertEquals(2, ts.retrieveTrailByName("Trail Three").getId());
+		assertEquals(3, ts.retrieveTrailByName("Trail Four").getId());
+		assertEquals(4, ts.retrieveTrailByName("Trail Five").getId());
+		assertEquals(5, ts.retrieveTrailByName("Trail Six").getId());
+		assertEquals(6, ts.retrieveTrailByName("Trail Seven").getId());
+		assertEquals(7, ts.retrieveTrailByName("Trail Eight").getId());
+		assertEquals(8, ts.retrieveTrailByName("Trail Nine").getId());
+		
+		//Name doesn't exist
+		assertEquals(null, ts.retrieveTrailByName("Trail None"));
+	}
 }
